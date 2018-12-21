f4656fe17c602008c8a650b2e34d38dd16ac5dfe
Kelvin Fichter <kelvinfichter@gmail.com>
2018-12-20 19:58:11 -0500

    Started PlasmaClient

---
+++ src/plasma.js
@@ -0,0 +1,19 @@
+class PlasmaClient {
+  sign (transaction) {
+    throw Error('Not implemented')
+  }
+
+  sendTransaction (transaction) {
+    throw Error('Not implemented')
+  }
+
+  getBalance () {
+    throw Error('Not implemented')
+  }
+
+  startExit () {
+    throw Error('Not implemented')
+  }
+}
+
+module.exports = PlasmaClient
